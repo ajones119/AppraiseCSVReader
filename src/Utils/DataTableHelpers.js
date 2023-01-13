2eadb4816749073d68795e2b49eceba76502bec9
ajones119 <ajones119@uco.edu>
2023-01-13 14:42:56 -0600

    fixing quarterly calc

--- src/Utils/DataTableHelpers.js
+++ src/Utils/DataTableHelpers.js
@@ -3,34 +3,12 @@ import { getPercentageChange } from "./calculations";
 export function getYearlyPercentageChangeRows(table, length) {
     for (let i = 0; i < length; i++) {
       if (i < Math.floor(table.length / 2)) {
-        const columnOne = table[i];
-        const columnTwo = table[Math.floor(table.length / 2) + i];
-        table[i].percentChangeAverageSalesPrice = getPercentageChange(
-          Number(columnOne.averageSalesPrice),
-          Number(columnTwo.averageSalesPrice)
-        );
-        table[i].percentChangeMedianSalesPrice = getPercentageChange(
-          Number(columnOne.medianSalesPrice),
-          Number(columnTwo.medianSalesPrice)
-        );
-        table[i].percentChangeAveragePricePerSquareFoot = getPercentageChange(
-          Number(columnOne.averagePricePerSquareFoot),
-          Number(columnTwo.averagePricePerSquareFoot)
-        );
-        table[i].percentChangeAverageSquareFeet = getPercentageChange(
-          Number(columnOne.averageSquareFeet),
-          Number(columnTwo.averageSquareFeet)
-        );
-        table[i].percentChangeMedianSquareFeet = getPercentageChange(
-          Number(columnOne.medianSquareFeet),
-          Number(columnTwo.medianSquareFeet)
-        );
+        const soonColumn = table[i];
+        const lateColumn = table[Math.floor(table.length / 2) + i];
+               getPercentageChangeRows(table, soonColumn, lateColumn, i);
+
       } else {
-        table[i].percentChangeAverageSalesPrice = "-";
-        table[i].percentChangeMedianSalesPrice = "-";
-        table[i].percentChangeAveragePricePerSquareFoot = "-";
-        table[i].percentChangeAverageSquareFeet = "-";
-        table[i].percentChangeMedianSquareFeet = "-";
+        getEmptyColumn(table, i);
       }
     }
 }
@@ -38,34 +16,43 @@ export function getYearlyPercentageChangeRows(table, length) {
 export function getQuarterlyPercentageChangeRows(table, length) {
   for (let i = 0; i < length; i++) {
     if (i < length - 1) {
-      const columnOne = table[i];
-      const columnTwo = table[i + 1];
-      table[i].percentChangeAverageSalesPrice = getPercentageChange(
-        Number(columnOne.averageSalesPrice),
-        Number(columnTwo.averageSalesPrice)
-      );
-      table[i].percentChangeMedianSalesPrice = getPercentageChange(
-        Number(columnOne.medianSalesPrice),
-        Number(columnTwo.medianSalesPrice)
-      );
-      table[i].percentChangeAveragePricePerSquareFoot = getPercentageChange(
-        Number(columnOne.averagePricePerSquareFoot),
-        Number(columnTwo.averagePricePerSquareFoot)
-      );
-      table[i].percentChangeAverageSquareFeet = getPercentageChange(
-        Number(columnOne.averageSquareFeet),
-        Number(columnTwo.averageSquareFeet)
-      );
-      table[i].percentChangeMedianSquareFeet = getPercentageChange(
-        Number(columnOne.medianSquareFeet),
-        Number(columnTwo.medianSquareFeet)
-      );
+      const soonColumn = table[i];
+      const lateColumn = table[i + 1];
+       getPercentageChangeRows(table, soonColumn, lateColumn, i);
     } else {
-      table[i].percentChangeAverageSalesPrice = "-";
-      table[i].percentChangeMedianSalesPrice = "-";
-      table[i].percentChangeAveragePricePerSquareFoot = "-";
-      table[i].percentChangeAverageSquareFeet = "-";
-      table[i].percentChangeMedianSquareFeet = "-";
+        getEmptyColumn(table, i);
     }
   }
+}
+
+function getPercentageChangeRows(table, soonColumn, lateColumn, index) {
+    table[index].percentChangeAverageSalesPrice = getPercentageChange(
+    Number(soonColumn.averageSalesPrice),
+    Number(lateColumn.averageSalesPrice)
+    );
+    table[index].percentChangeMedianSalesPrice = getPercentageChange(
+    Number(soonColumn.medianSalesPrice),
+    Number(lateColumn.medianSalesPrice)
+    );
+    table[index].percentChangeAveragePricePerSquareFoot =
+    getPercentageChange(
+        Number(soonColumn.averagePricePerSquareFoot),
+        Number(lateColumn.averagePricePerSquareFoot)
+    );
+    table[index].percentChangeAverageSquareFeet = getPercentageChange(
+    Number(soonColumn.averageSquareFeet),
+    Number(lateColumn.averageSquareFeet)
+    );
+    table[index].percentChangeMedianSquareFeet = getPercentageChange(
+    Number(soonColumn.medianSquareFeet),
+    Number(lateColumn.medianSquareFeet)
+    );
+}
+
+function getEmptyColumn(table, index) {
+    table[index].percentChangeAverageSalesPrice = "-";
+    table[index].percentChangeMedianSalesPrice = "-";
+    table[index].percentChangeAveragePricePerSquareFoot = "-";
+    table[index].percentChangeAverageSquareFeet = "-";
+    table[index].percentChangeMedianSquareFeet = "-";
 }
